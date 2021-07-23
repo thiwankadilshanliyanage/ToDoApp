40b2fcad3bc961b8489480ad037e861d30e07f3a
thiwankadilshanliyanage <thiwankadilshanhub98@gmail.com>
2021-07-23 11:22:00 +0530

    ToDoApp

--- app/src/main/java/com/example/todoapp/RecycleItemTouchHelper.java
+++ app/src/main/java/com/example/todoapp/RecycleItemTouchHelper.java
@@ -3,6 +3,7 @@ package com.example.todoapp;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.view.View;
@@ -68,6 +69,35 @@ public class RecycleItemTouchHelper extends ItemTouchHelper.SimpleCallback {
 
         if(dX>0){
             icon = ContextCompat.getDrawable(adapter.getContext(),R.drawable.ic_baseline_edit);
+            background = new ColorDrawable(ContextCompat.getColor(adapter.getContext(),R.color.colorPrimaryDark));
+        }else {
+            icon = ContextCompat.getDrawable(adapter.getContext(),R.drawable.ic_baseline_delete);
+            background = new ColorDrawable(Color.RED);
         }
+        int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) /2;
+        int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) /2;
+        int iconBottom = iconTop + icon.getIntrinsicHeight();
+
+        if(dX>0){
+            int iconLeft = itemView.getLeft() + iconMargin;
+            int iconRight = itemView.getLeft() + iconMargin + icon.getIntrinsicWidth();
+            icon.setBounds(iconLeft,iconTop,iconRight,iconBottom);
+
+            background.setBounds(itemView.getLeft(),itemView.getTop(),
+                    itemView.getLeft()+((int)dX) + backgroundCornerOffset, itemView.getBottom());
+        }
+        else if(dX<0){//Swiping to the left
+            int iconLeft = itemView.getRight() - iconMargin -icon.getIntrinsicWidth();
+            int iconRight = itemView.getRight() - iconMargin;
+            icon.setBounds(iconLeft,iconTop,iconRight,iconBottom);
+
+            background.setBounds(itemView.getRight() + ((int)dX) - backgroundCornerOffset,itemView.getTop(),
+                    itemView.getRight(), itemView.getBottom());
+        }else {
+            background.setBounds(0,0,0,0);
+        }
+        background.draw(c);
+        icon.draw(c);
     }
-}
+    }
+
